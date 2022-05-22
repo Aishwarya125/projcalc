894c28700cbc99df64d2db54c40615f9f7ed09f3
Aishwarya125 <aishwaryaharini@gmail.com>
2022-05-22 21:21:26 +0530

    Task calculate

---
+++ src/org/calc/Calculate.java
@@ -0,0 +1,21 @@
+package org.calc;
+
+public class Calculate {
+
+	public static void main(String[] args) 
+	{
+		
+		
+		{
+
+	System.out.println("main method");
+
+	}
+	}
+
+	private static void add() {
+		// TODO Auto-generated method stub
+		System.out.println("adddition");
+	}
+	 
+}
